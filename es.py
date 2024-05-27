18dc276277fb079db92f5844a08420543c3d1657
Jun Hu <hu.jun@iisgalvanimi.edu.it>
2024-05-27 09:06:57 +0000

    work in progress

--- es.py
+++ es.py
@@ -6,27 +6,14 @@ df = pd.read_csv('/workspace/Test/data/data.csv')
 
 @app.route('/')
 def homepage():
-    nazioni = df['country'].drop_duplicates()
-    return render_template('homepage.html', elenco=nazioni)
+    nazioni = sorted(list(set(df['country'])))
+    return render_template('homepage.html', elenco = nazioni)
 
-from flask import Flask, render_template, request
-import pandas as pd
-
-app = Flask(__name__)
-df = pd.read_csv('/workspace/Test/data/data.csv')
-
-@app.route('/')
-def homepage():
-    nazioni = df['country'].unique()
-    return render_template('homepage.html', elenco=nazioni)
-
-@app.route('/elencoCitta', methods=['POST'])
-def elencoCitta():
-    nazioneSelezionata = request.form.get('nazione')
-    citta = df[df['country'] == nazioneSelezionata].groupby('city').size().sort_values(ascending=False)
-    elenco = pd.DataFrame({'city': citta.index, 'num_clients': citta.values})
-    print(elenco)
-    return render_template('elencoCitta.html', elenco=elenco)
+@app.route('/elencoCItta/<nazione>', methods=['GET'])
+def elencoCitta(nazione):
+    nazioneSelezionata = df[df['country'] == nazione]
+    citta = nazioneSelezionata['city'].value_counts().sort_values(ascending = False)
+    return render_template('elencoCitta.html', elenco = citta)
 
 @app.route('/elencoClienti', methods=['POST'])
 def elencoClienti():
