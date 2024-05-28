35246adde3a1b4d750eaa102b61d57a2c3123aa4
Jun Hu <hu.jun@iisgalvanimi.edu.it>
2024-05-28 06:40:41 +0000

    not finished

--- es.py
+++ es.py
@@ -6,23 +6,20 @@ df = pd.read_csv('/workspace/Test/data/data.csv')
 
 @app.route('/')
 def homepage():
-    nazioni = sorted(list(set(df['country'])))
+    nazioni = sorted(list(set(df['Country'])))
     return render_template('homepage.html', elenco = nazioni)
 
 @app.route('/elencoCItta/<nazione>', methods=['GET'])
 def elencoCitta(nazione):
-    nazioneSelezionata = df[df['country'] == nazione]
-    citta = nazioneSelezionata['city'].value_counts().sort_values(ascending = False)
+    nazioneSelezionata = df[df['Country'] == nazione]
+    citta = nazioneSelezionata['City'].value_counts().sort_values(ascending = False)
     return render_template('elencoCitta.html', elenco = citta)
 
-@app.route('/elencoClienti', methods=['POST'])
+@app.route('/elencoClienti', methods=['GET','POST'])
 def elencoClienti():
     cittaSelezionata = request.form.get('citta')
-    print(cittaSelezionata)
-    elenco = df[df['city'] == cittaSelezionata]
-    elenco = elenco.to_html()
-    return render_template('elencoClienti.html', elenco = elenco)
-
+    infoClienti = df[df['City'] == cittaSelezionata]
+    return render_template('elencoClienti.html', elenco = infoClienti.to_html())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port = 3245, debug = True)
